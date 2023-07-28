6ea5d82b5a71f73f81f0e49411d0f0a034a95a01
Ahmad-Haymour <92795077+Ahmad-Haymour@users.noreply.github.com>
2023-07-28 13:55:02 +0200

    -

--- server/app.js
+++ server/app.js
@@ -32,8 +32,8 @@ mongoose
 });
 
 const corsConfig = {
-  // origin: 'http://127.0.0.1:3000',
-  origin: "*",
+  origin: 'http://127.0.0.1:3000',
+  // origin: "*",
   credentials: true,
   // withCredentials: true,
   // methods: ["GET", "POST", "PUT", "DELETE"]
