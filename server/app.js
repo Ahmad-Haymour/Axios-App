aba07e0a8dca56bf8182121bc409e0684b09c5b2
Ahmad-Haymour <92795077+Ahmad-Haymour@users.noreply.github.com>
2023-08-05 16:42:40 +0200

    initial commit

--- server/app.js
+++ server/app.js
@@ -33,9 +33,9 @@ mongoose
 });
 
 const corsConfig = {
-  // origin: 'http://127.0.0.1:3000',
-  // origin: "*",
-  origin: "https://axios-events.onrender.com",
+    origin: "*",
+    // origin: 'http://127.0.0.1:3000',
+    //   origin: "https://axios-events.onrender.com",
   credentials: true,
 }
 
